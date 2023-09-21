3ae860a40033f85ad3f8ce649ac4da4da1f65e26
Michael Muchiri <muchirimichael2222@gmail.com>
2023-09-21 09:09:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(passengerStreet){
+    const hqStreet=42
+    if (passengerStreet>hqStreet){
+     return (passengerStreet-hqStreet)
+    }
+    else{
+        return(hqStreet-passengerStreet)
+    } 
+
+}
+
+    
+function distanceFromHqInFeet(passengerStreet){
+          return distanceFromHqInBlocks(passengerStreet)*264
+}
+
+function distanceTravelledInFeet(starting,destination){
+    if(destination>starting){
+        return((destination-starting)*264)
+    } 
+    else{
+        return((starting-destination)*264)
+    }
+}
+
+function calculatesFarePrice(starting,destination){
+    const distanceInFeet=distanceTravelledInFeet(starting,destination)
+    if(distanceInFeet<=400){
+        return(0)
+    }
+    if(distanceInFeet>400,distanceInFeet<=2000){
+        return((((distanceInFeet-400)*2))/100)
+    }
+    if(distanceInFeet>2000,distanceInFeet<=2500){
+        return(25)
+    }
+    if(distanceInFeet>2500){
+        return('cannot travel that far')
+    }
+}
